87cedd65c199d47f84c5a88aaa0fcc7f78789efb
Alex <alexgmiller00@gmail.com>
2022-06-10 14:25:02 -0700

    Adds comments

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
+/* Alex Miller, CSC 15, Summer 2022
+ This program prints out a drawing of a character by calling four different methods that use println statements.
+ */
+/* my grade for this assignment is 100 since my program meets all of the rubric requirements*/
 public class App {
     public static void main(String[] args) throws Exception {
-        drawHat();
-        drawFace();
-        drawBody();
-        drawBottom();
+        drawHat(); // top portion of the drawing
+        drawFace(); // face of the person in drawing
+        drawBody(); // body of person in drawing
+        drawBottom(); // lower portion of the drawing
     }
 
     public static void drawHat() {
