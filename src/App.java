90246a1cdc4a38941f7aaaa9a6082dfc2f8b0ff0
Alex <alexgmiller00@gmail.com>
2022-06-10 14:02:14 -0700

    Initializes

---
+++ src/App.java
@@ -0,0 +1,51 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        drawHat();
+        drawFace();
+        drawBody();
+        drawBottom();
+    }
+
+    public static void drawHat() {
+        System.out.println("          /\"\\");
+        System.out.println("         /\"_\"\\");
+        System.out.println("        /\"_\"_\"\\   ");
+        System.out.println("       /\"_\"_\"_\"\\");
+        System.out.println("      /\"_\"_\"_\"_\"\\ ");
+        System.out.println("     /\"_\"_\"_\"_\"_\"\\ ");
+        System.out.println("      |||||||||||  ");
+    }
+
+    public static void drawFace() {
+        System.out.println("     |  ~      ~  | ");
+        System.out.println("    @|   O /\\  O  |@");
+        System.out.println("     |   /----    | ");
+        System.out.println("     |____________| ");
+        System.out.println("          | |       ");
+    }
+
+    public static void drawBody() {
+        System.out.println("    ______| |______  ");
+        System.out.println("   |               |");
+        System.out.println("  /|    MILLER     |\\");
+        System.out.println(" / |               | \\");
+        System.out.println("*  |_______________|  *");
+        System.out.println("      | |     | |");
+        System.out.println("      | |     | |");
+        System.out.println("      | |     | |");
+        System.out.println("      / \\     / \\");
+        System.out.println("     |____\\  |____\\");
+    }
+
+    public static void drawBottom() {
+        System.out.println("       \\/      \\/");
+        System.out.println("       /\\      /\\");
+        System.out.println("       \\/      \\/");
+        System.out.println("     \\\"_\"_\"_\"_\"_\"/");
+        System.out.println("      \\\"_\"_\"_\"_\"/");
+        System.out.println("       \\\"_\"_\"_\"/");
+        System.out.println("        \\\"_\"_\"/");
+        System.out.println("         \\\"_\"/");
+        System.out.println("          \\\"/");
+    }
+}
